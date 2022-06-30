99778a12802e1275cdf200eb05e79dbfcf6e829b
Joshua Cheung <contact.cjoshua@gmail.com>
2022-06-29 17:27:31 -0700

    add dashboard-counts feature

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,10 @@ class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
 
-
   def show
+
+    @all_products = Product.all
+    @all_categories = Category.all
+
   end
 end
