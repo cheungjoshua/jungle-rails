b1d0fd5386e95d6af9aa416e0e45c25f6c490564
Joshua Cheung <contact.cjoshua@gmail.com>
2022-06-30 12:03:14 -0700

    finish admin/category controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,11 +1,28 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+                               password: ENV["HTTP_BASIC_PASSWORD"]
+
   def index
     @categories = Category.all
   end
 
   def new
+    @categories = Category.new
   end
 
   def create
+    @categories = Category.new(param_category)
+
+    if @categories.save
+      redirect_to %i[admin categories], notice: "Category created!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_category
+    params.require(:category).permit(:name, :created_at, :updated_at)
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,6 @@
 class Admin::ProductsController < ApplicationController
-  
-  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
-
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+                               password: ENV["HTTP_BASIC_PASSWORD"]
 
   def index
     @products = Product.order(id: :desc).all
@@ -15,7 +14,7 @@ class Admin::ProductsController < ApplicationController
     @product = Product.new(product_params)
 
     if @product.save
-      redirect_to [:admin, :products], notice: 'Product created!'
+      redirect_to %i[admin products], notice: "Product created!"
     else
       render :new
     end
@@ -24,7 +23,7 @@ class Admin::ProductsController < ApplicationController
   def destroy
     @product = Product.find params[:id]
     @product.destroy
-    redirect_to [:admin, :products], notice: 'Product deleted!'
+    redirect_to %i[admin products], notice: "Product deleted!"
   end
 
   private
@@ -39,5 +38,4 @@ class Admin::ProductsController < ApplicationController
       :price
     )
   end
-
 end
