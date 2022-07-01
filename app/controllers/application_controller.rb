f75b4fbcccebb378ba4be4afeb618e320d92c903
Joshua Cheung <contact.cjoshua@gmail.com>
2022-06-30 18:23:31 -0700

    Add action in sessions controller and app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  
+
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    redirect_to "/login" unless current_user
+  end
 
   private
 
@@ -12,15 +21,19 @@ class ApplicationController < ActionController::Base
   helper_method :cart
 
   def enhanced_cart
-    @enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
+    @enhanced_cart ||=
+      Product
+        .where(id: cart.keys)
+        .map { |product| { product: product, quantity: cart[product.id.to_s] } }
   end
   helper_method :enhanced_cart
 
   def cart_subtotal_cents
-    enhanced_cart.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum
+    enhanced_cart
+      .map { |entry| entry[:product].price_cents * entry[:quantity] }
+      .sum
   end
   helper_method :cart_subtotal_cents
-
 
   def update_cart(new_cart)
     cookies[:cart] = {
